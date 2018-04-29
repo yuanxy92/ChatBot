ae5d7287b360700f6c89762d90f94e6dd88a4b3d
ShaneYuan <yuanxy92@gmail.com>
2018-04-29 15:55:45 +0800

    add rasa example

---
+++ rasa_chatbot_python3/bot.py
@@ -0,0 +1,137 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import logging
+import warnings
+
+from rasa_core import utils
+from rasa_core.actions import Action
+from rasa_core.agent import Agent
+from rasa_core.channels.console import ConsoleInputChannel
+from rasa_core.interpreter import RasaNLUInterpreter
+from rasa_core.policies.keras_policy import KerasPolicy
+from rasa_core.policies.memoization import MemoizationPolicy
+from rasa_core.events import SlotSet
+
+logger = logging.getLogger(__name__)
+
+import os
+import socket
+import time
+
+class RestaurantAPI:
+    def search(self, info):
+        return "papi's pizza place"
+
+
+class ActionSearchRestaurants(Action):
+    def name(self):
+        return 'action_search_restaurants'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("looking for restaurants")
+        restaurant_api = RestaurantAPI()
+        restaurants = restaurant_api.search(tracker.get_slot("cuisine"))
+        return [SlotSet("matches", restaurants)]
+
+
+class ActionSuggest(Action):
+    def name(self):
+        return 'action_suggest'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("here's what I found:")
+        dispatcher.utter_message(tracker.get_slot("matches"))
+        dispatcher.utter_message("is it ok for you? "
+                                 "hint: I'm not going to "
+                                 "find anything else :)")
+        return []
+
+
+class RestaurantPolicy(KerasPolicy):
+    def model_architecture(self, num_features, num_actions, max_history_len):
+        """Build a Keras model and return a compiled model."""
+        from keras.layers import LSTM, Activation, Masking, Dense
+        from keras.models import Sequential
+
+        n_hidden = 32  # size of hidden layer in LSTM
+        # Build Model
+        batch_shape = (None, max_history_len, num_features)
+
+        model = Sequential()
+        model.add(Masking(-1, batch_input_shape=batch_shape))
+        model.add(LSTM(n_hidden, batch_input_shape=batch_shape))
+        model.add(Dense(input_dim=n_hidden, output_dim=num_actions))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        logger.debug(model.summary())
+        return model
+
+
+def train_dialogue(domain_file="restaurant_domain.yml",
+                   model_path="models/dialogue",
+                   training_data_file="data/babi_stories.md"):
+    agent = Agent(domain_file,
+                  policies=[MemoizationPolicy(), RestaurantPolicy()])
+
+    agent.train(
+            training_data_file,
+            max_history=3,
+            epochs=400,
+            batch_size=100,
+            validation_split=0.2
+    )
+
+    agent.persist(model_path)
+    return agent
+
+
+def train_nlu():
+    from rasa_nlu.converters import load_data
+    from rasa_nlu.config import RasaNLUConfig
+    from rasa_nlu.model import Trainer
+
+    training_data = load_data('data/franken_data.json')
+    trainer = Trainer(RasaNLUConfig("nlu_model_config.json"))
+    trainer.train(training_data)
+    model_directory = trainer.persist('models/nlu/', fixed_model_name="current")
+
+    return model_directory
+
+
+def run(serve_forever=True):
+    interpreter = RasaNLUInterpreter("models/nlu/default/current")
+    agent = Agent.load("models/dialogue", interpreter=interpreter)
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.bind(('127.0.0.1',54377))
+    sock.listen(5)
+    print("start server")
+    while True:
+        try:
+            connection, address = sock.accept()
+            print("client ip is:", address)
+            buf = connection.recv(40960)
+            print(buf)
+            buf_as_bytes = buf.decode()
+            out_as_bytes = agent.handle_message(buf_as_bytes)
+            print(out_as_bytes)
+            out = str.encode(out_as_bytes[0])
+            connection.sendall(out)
+            print(out)
+            connection.close()
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print("exit")
+            connection.close()
+            break
+
+
+if __name__ == '__main__':
+    run()
--- rasa_chatbot_python3/chat.py
+++ rasa_chatbot_python3/chat.py
@@ -1,30 +1,27 @@
-import aiml
 import os
-os.chdir('./alice')
-alice = aiml.Kernel()
-alice.learn("startup.xml")
-print alice.respond('LOAD ALICE')
-print alice.respond('hello')
-
 import socket
 import time
+
+from rasa_core.agent import Agent
+agent = Agent.load("examples/restaurantbot/models/dialogue",
+    interpreter="examples/restaurantbot/models/nlu/current")
+agent.handle_message("hello")
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.bind(('127.0.0.1',54377))
 sock.listen(5)
-print "start server"
+print("start server")
 while True:
     try:
         connection, address = sock.accept()
-        print "client ip is:", address
+        print("client ip is:", address)
         buf = connection.recv(40960)
-        out = alice.respond(buf)
+        out = agent.handle_message(buf)
         connection.sendall(out)
-        print out
+        print(out)
         connection.close()
         time.sleep(1)
     except KeyBoardInterrupt:
-        print "exit"
         connection.close()
         break
 
